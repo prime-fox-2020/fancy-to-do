1979db1553b33593110dccbeb61164e86ec4c20d
Sujackson <su_jackson99@yahoo.com>
2020-04-28 12:42:23 +0700

    authen/author

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-const router=require('express').Router()
-const todosRouter=require('./todos')
-const userRouter=require('./user')
-
-router.use('/todos',todosRouter)
-router.use('/user',userRouter)
-
-
-module.exports=router
