c9e120b644035541713604ae630b5adaad597dc2
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-04-28 10:49:24 +0700

    test documentation

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const Controller = require('../controllers/todoController');
 
+const authentication = (req, res, next) => {
+    
+}
+
+const authorization = (req, res, next) => {
+    
+}
+
 router.post('/', Controller.create);
 router.get('/', Controller.read);
 router.get('/:id', Controller.readById);
