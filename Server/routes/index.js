693f20a8eb276851b18d525cf8c525e8ed84c7fe
unknown <sonymartha4@gmail.com>
2020-04-29 12:38:26 +0700

    add 3rd app endpoint /waktusalat

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const todo = require('./todo')
 const user = require('./user')
+const muslimSalat = require('./muslimSalat')
 
 // router.get('/', (req,res) => {
 //     res.send('hi ini router')
@@ -8,6 +9,7 @@ const user = require('./user')
 
 router.use('/', user)
 router.use('/todos', todo)
+router.use('/jadwalsalat', muslimSalat)
 
 
 module.exports = router
