1d7c3af8700ecdde7fcb9e39c3ac1375e966e9e3
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-04-29 08:24:33 +0700

    sudh-berhasil-tapi-3rd-API

--- server/controllers/charityController.js
+++ server/controllers/charityController.js
@@ -5,11 +5,11 @@ class charityController{
      axios({
        method: 'GET',
        url : `http://data.orghunter.com/v1/categories?user_key=14058a8f469257c1a36ca66c5985825d`
-      //  http://data.orghunter.com/v1/categories?user_key=${process.env.API-KEY}=INI gagal juga kak 
+      //  http://data.orghunter.com/v1/categories?user_key=${process.env.API-KEY}=INI gagal  kak 
      })
      .then(response=>{
        res.status(200).json({
-         data: response
+         data: response.data
        })
      })
      .catch(err=>{
