0cdc3688dece4e8fdc8d5f03e8ce36e9ee63b4e6
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-05-01 14:48:47 +0700

    connect login and register features to server

--- client/index.js
+++ client/index.js
@@ -1,47 +1,54 @@
 // import { json } from "express";
 function onSignIn(googleUser) {
-    // console.log(googleUser.getBasicProfile())
-    // var profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token;
 
-    // alert(id_token)
     $.ajax({
         method: "POST",
         url: "http://localhost:3000/users/google-signIn",
         data: { id_token }
-      })
-        .done(function( msg ) {
-        console.log(msg.access_token)
-        localStorage.setItem('access_token', msg.access_token);
+    })
+        .done(function (msg) {
+            console.log(msg.access_token)
+            localStorage.setItem('access_token', msg.access_token);
         });
 }
+
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+        console.log('User signed out.');
     });
-  }
+}
+
 
 $(document).ready(function () {
     let isLogin = false;
-
     $("#home").hide()
     $("#form_register").hide()
-    $("#login" || "#logout").click(function (e) {
+
+
+    $("#login" || "#logout" || "#login_google").click(function (e) {
         e.preventDefault();
+        const email = $("#email").val();
+        const password = $("#password").val();
+        
         if (isLogin) {
             $("#home").hide();
             $("#form").show();
             isLogin = false;
         } else {
-            $("#form").hide();
-            $("#home").show();
-            isLogin = true;
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost:3000/users/login',
+                dataType: 'json',
+                data: { email, password }
+            })
+                .done(function () {
+                    $("#form").hide();
+                    $("#home").show();
+                    isLogin = true;
+                })
         }
     })
 
@@ -51,10 +58,33 @@ $(document).ready(function () {
         $("#form_register").show()
     })
 
-    $("submit_register").click(function (e) {
+    $("#submit_register").click(function (e) {
         e.preventDefault();
-        $("#form_register").hide()
-        $("#form_login").show()
+
+        let result = {
+            first_name: $("#first_name").val(),
+            last_name: $("#last_name").val(),
+            email: $("#email_form").val(),
+            password: $("#password_form").val()
+        }
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/users/register',
+            dataType: 'json',
+            data: result
+        })
+            .done(function () {
+                console.log('SUCCESS')
+                $("#form_register").hide()
+                $("#form_login").show()
+            })
+            .fail(function (err) {
+                console.log(err)
+                console.log('ERRR')
+            })
+            .always(function () {
+                console.log('ALWAYS')
+            })
     })
 
     $("#input_data").click(function (e) {
@@ -69,48 +99,6 @@ $(document).ready(function () {
             </li>`);
             $("#input_todo").val("");
         }
-        // console.log($("i").get())
-        $("#check").remove();
     })
 
-    $("#submit_register").click(function (e) {
-        e.preventDefault();
-
-        const data = $("#form_register").serializeArray()
-        let result = {}
-        data.forEach(el => {
-            result[el.name] = el.value
-        });
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/users/register',
-            dataType: 'json',
-            data: result
-        })
-            .done(function () {
-                console.log('SUCCESS')
-            })
-            .fail(function (err) {
-                console.log(err)
-                console.log('ERRR')
-            })
-            .always(function () {
-                console.log('ALWAYS')
-            })
-    })
-
-    // $.ajax({
-    //     type: 'GET',
-    //     url: 'http://localhost:3000/todos'
-    // })
-    // .done(function (){
-    //     console.log('SUCCESS')
-    // })
-    // .fail(function (err){
-    //     console.log('FAILED')
-    //     console.log(err)
-    // })
-    // .always(function (){
-    //     console.log('ALWAYS')
-    // })
 })
