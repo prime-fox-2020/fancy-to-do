ee38df0806178635ed46775480c8fc6141c89638
alamrumambi <alam.abumaryam@gmail.com>
2020-05-02 10:43:15 -0700

    fix crud

--- client/index.js
+++ client/index.js
@@ -10,6 +10,7 @@ function onSignIn(googleUser) {
             localStorage.setItem('access_token', data.access_token);
             $("#login-form").hide();
             $(".alert-danger").hide();
+            $("#main-navbar").show();
             showData(localStorage.getItem('access_token'));
         })
         .fail(err => {
@@ -29,12 +30,14 @@ function signOut() {
     $("#show-data").hide();
     $(".alert-danger").hide();
     $(".alert-success").hide();
+    $("#main-navbar").hide();
+    $("input-password").val("");
 }
 
 //read data todos
 const showData = (token) => {
     $.ajax({
-        type: "GET",
+        method: "GET",
         url: "http://localhost:3000/todos",
         headers: { access_token: token }
     })
@@ -47,13 +50,121 @@ const showData = (token) => {
                 <td>${data[i].description}</td>
                 <td>${data[i].due_date}</td>
                 <td>${data[i].status}</td>
-                <td>${data[i].UserId}</td>
+                <td>
+                    <button type="button" onclick="editTodos(${data[i].id});" class="btn btn-primary">Edit</button>&nbsp;
+                    <button type="button" onclick="deleteTodos(${data[i].id});" class="btn btn-danger">Delete</button>
+                </td>
                 </tr>`
             }
             table.html(todos);
         })
     $("#show-data").show();
 }
+
+//post / put todos
+$("#input-data").submit((e) => {
+    e.preventDefault();
+    const titleForm = $("#form-input-title").text();
+    const access_token = localStorage.getItem("access_token");
+    const id = $("#id").val();
+    const title = $("#title").val();
+    const description = $("#description").val();
+    const due_date = $("#due-date").val();
+
+    //post data
+    if (titleForm == 'Add New Todo') {
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/todos",
+            headers: { access_token },
+            data: { title, description, due_date }
+        })
+            .done(data => {
+                $("#input-data").hide();
+                $("#show-data").show();
+                $(".alert-danger").hide();
+                showData(localStorage.getItem('access_token'));
+            })
+            .fail(err => {
+                $(".alert-danger").text(err.responseJSON.message);
+                $(".alert-danger").show();
+            })
+    }
+    else { //put data
+        $.ajax({
+            type: "PUT",
+            url: `http://localhost:3000/todos/${id}`,
+            headers: { access_token },
+            data: { title, description, due_date }
+        })
+            .done(data => {
+                $("#input-data").hide();
+                $("#show-data").show();
+                $(".alert-danger").hide();
+                showData(localStorage.getItem('access_token'));
+            })
+            .fail(err => {
+                $(".alert-danger").text(err.responseJSON.message);
+                $(".alert-danger").show();
+            })
+    }
+})
+
+//goto form add
+const addTodos = () => {
+    $("#form-input-title").text("Add New Todo");
+    $("#input-submit").text("Submit");
+    $("#title").val("");
+    $("#description").val("");
+    $("#due-date").val("");
+    $("#show-data").hide();
+    $("#input-data").show();
+}
+
+//goto form edit
+const editTodos = (params) => {
+    const access_token = localStorage.getItem('access_token');
+    const url = `http://localhost:3000/todos/${params}`;
+    $.ajax({
+        method: "GET",
+        url,
+        headers: { access_token }
+    })
+        .done(data => {
+            $("#form-input-title").text("Update Todo");
+            $("#input-submit").text("Update");
+            $("#id").val(data.id);
+            $("#title").val(data.title);
+            $("#description").val(data.description);
+            $("#due-date").val(data.due_date);
+            $("#show-data").hide();
+            $("#input-data").show();
+        })
+}
+
+//delete data
+const deleteTodos = (params) => {
+    var x = confirm("Are you sure you want to delete?");
+    if (x) {
+        const access_token = localStorage.getItem('access_token');
+        const url = `http://localhost:3000/todos/${params}`;
+        $.ajax({
+            type: "DELETE",
+            url,
+            headers: { access_token }
+        })
+            .done(data => {
+                showData(access_token);
+            })
+    }
+    else return false;
+}
+
+//cancel input
+$("#cancel").click(() => {
+    $("#input-data").hide();
+    $("#show-data").show();
+})
 
 //jquery
 $(document).ready(() => {
@@ -62,7 +173,9 @@ $(document).ready(() => {
     $("#register-button").css("color", "blue");
     $(".alert-danger").hide();
     $(".alert-success").hide();
-    signOut();
+    $("#main-navbar").hide();
+    $("#input-data").hide();
+    // signOut();
 
     //To register page
     $("#register-button").click(() => {
@@ -100,6 +213,7 @@ $(document).ready(() => {
                     localStorage.setItem('access_token', data.access_token);
                     $("#login-form").hide();
                     $(".alert-danger").hide();
+                    $("#main-navbar").show();
                     showData(localStorage.getItem('access_token'));
                 })
                 .fail(err => {
