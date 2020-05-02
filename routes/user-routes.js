f47ca5824fcf745511e1f0d6ec526c02168b64a7
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-05-02 17:32:53 +0700

    Delete user-routes.js

--- routes/user-routes.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router()
-const userController = require('../controllers/userController.js')
-
-
-routes.post('/register',userController.register)
-routes.post('/login',userController.login)
-module.exports = routes
